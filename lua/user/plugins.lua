d95da57b1a43f2b034d318622c5cc3703031b599
Lalo <lalo.g@bytedance.com>
2023-01-14 15:14:02 +0800

    new plug version

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,8 +44,8 @@ packer.init {
 -- Install your plugins here
 return packer.startup(function(use)
   -- My plugins here
-  use { "wbthomason/packer.nvim", commit = "6afb67460283f0e990d35d229fd38fdc04063e0a" } -- Have packer manage itself
-  use { "nvim-lua/plenary.nvim", commit = "4b7e52044bbb84242158d977a50c4cbcd85070c7" } -- Useful lua functions used by lots of plugins
+  use { "wbthomason/packer.nvim" } -- Have packer manage itself
+  use { "nvim-lua/plenary.nvim" } -- Useful lua functions used by lots of plugins
   use { "windwp/nvim-autopairs", commit = "4fc96c8f3df89b6d23e5092d31c866c53a346347" } -- Autopairs, integrates with both cmp and treesitter
   use { "numToStr/Comment.nvim" }
   use { "JoosepAlviste/nvim-ts-context-commentstring" }
@@ -56,8 +56,8 @@ return packer.startup(function(use)
   use { "nvim-lualine/lualine.nvim" }
   use { "akinsho/toggleterm.nvim" }
   use { "lewis6991/impatient.nvim", commit = "b842e16ecc1a700f62adb9802f8355b99b52a5a6" }
-  use { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
-  use { "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
+  use { "lukas-reineke/indent-blankline.nvim" }
+  use { "goolord/alpha-nvim" }
   use { "junegunn/vim-easy-align" }
   use { "wellle/targets.vim" }
   use { "folke/which-key.nvim" }
@@ -79,15 +79,14 @@ return packer.startup(function(use)
   use { "AndrewRadev/switch.vim" }
   -- use { "edluffy/hologram.nvim", config = function() require'hologram'.setup{ auto_display = true } end }
   use { "renerocksai/calendar-vim" }
+  use { "renerocksai/telekasten.nvim" }
   use { "dhruvasagar/vim-table-mode" }
-
   use { "github/copilot.vim", config = function() vim.g.copilot_no_tab_map = true end }
 
   -- local plugins
   use { "~/git/nvim/im-select.nvim" }
   use { "~/git/nvim/buildit.nvim" }
   use { "~/git/nvim/session-lens" }
-  use { "~/git/nvim/telekasten.nvim" }
   use { "~/git/nvim/project.nvim" }
 
   -- Colorschemes
@@ -112,14 +111,14 @@ return packer.startup(function(use)
   use { "onsails/lspkind.nvim" }
 
   -- snippets
-  use { "L3MON4D3/LuaSnip", commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84" } --snippet engine
-  use { "rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" } -- a bunch of snippets to use
+  use { "L3MON4D3/LuaSnip" } --snippet engine
+  use { "rafamadriz/friendly-snippets" } -- a bunch of snippets to use
 
   -- LSP
-  use { "neovim/nvim-lspconfig", commit = "f11fdff7e8b5b415e5ef1837bdcdd37ea6764dda" } -- enable LSP
-  use { "williamboman/mason.nvim", commit = "bfc5997e52fe9e20642704da050c415ea1d4775f"}
-  use { "williamboman/mason-lspconfig.nvim", commit = "0eb7cfefbd3a87308c1875c05c3f3abac22d367c" }
-  use { "jose-elias-alvarez/null-ls.nvim", commit = "c0c19f32b614b3921e17886c541c13a72748d450" } -- for formatters and linters
+  use { "neovim/nvim-lspconfig" } -- enable LSP
+  use { "williamboman/mason.nvim" }
+  use { "williamboman/mason-lspconfig.nvim" }
+  use { "jose-elias-alvarez/null-ls.nvim" } -- for formatters and linters
 
   -- Telescope
   use { "nvim-telescope/telescope.nvim" }
@@ -136,11 +135,11 @@ return packer.startup(function(use)
   }
 
   -- Git
-  use { "lewis6991/gitsigns.nvim", commit = "f98c85e7c3d65a51f45863a34feb4849c82f240f" }
+  use { "lewis6991/gitsigns.nvim" }
 
   -- DAP
-  use { "mfussenegger/nvim-dap", commit = "6b12294a57001d994022df8acbe2ef7327d30587" }
-  use { "rcarriga/nvim-dap-ui", commit = "1cd4764221c91686dcf4d6b62d7a7b2d112e0b13" }
+  use { "mfussenegger/nvim-dap" }
+  use { "rcarriga/nvim-dap-ui" }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
