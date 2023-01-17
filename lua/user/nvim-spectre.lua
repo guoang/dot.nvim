293010836c35c2fb79d78691ab3a71f6a0bf6a73
Lalo <lalo.g@bytedance.com>
2023-01-17 16:10:19 +0800

    spectre toggle word

--- lua/user/nvim-spectre.lua
+++ lua/user/nvim-spectre.lua
@@ -4,15 +4,22 @@ if not status_ok then
 end
 
 spectre.setup({
+  mapping = {
+    ["toggle_word_boundary"] = {
+      map = "tw",
+      cmd = "<cmd>lua require('spectre').change_options('word')<CR>",
+      desc = "toggle word boundary",
+    },
+  },
   find_engine = {
     rg = {
       options = {
         word = {
           value = "--word-regexp",
           desc = "Only show matches surrounded by word boundaries.",
-          icon = "[W]"
-        }
-      }
-    }
-  }
+          icon = "[W]",
+        },
+      },
+    },
+  },
 })
