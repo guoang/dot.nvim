0d7cf2b0dfec738974d67e0cf7eff9d93c9e633d
Lalo <lalo.g@bytedance.com>
2023-01-18 23:02:16 +0800

    runit

--- lua/user/runit.lua
+++ lua/user/runit.lua
@@ -3,8 +3,8 @@ if not status_ok then
   return
 end
 
-local xsolution_cpp_modules = { "xnet", "xlog", "chrono", "xtimer" }
-local xsolution_py_modules = { "xhttp", "xserver" }
+local xsolution_cpp_modules = { "xnet", "xlog", "chrono", "xtimer", "cpython", "mimalloc", "xsolution" }
+local xsolution_py_modules = { "xhttp", "xserver", "pyrc", "sop" }
 
 for _, module in ipairs(xsolution_cpp_modules) do
   runit.setup({
@@ -27,7 +27,7 @@ for _, module in ipairs(xsolution_py_modules) do
   runit.setup({
     project = {
       [module] = {
-        install = "cd ~/work/xbuild/_deps/" .. module .. "-build && make -j8 install",
+        install = "cd ~/work/xbuild/_deps/${__proj_name__}-build && make -j8 install",
         all = { "#install" },
       },
     },
