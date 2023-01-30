44432a78e841c0ec4c57fe172be530412a4cb1a8
Lalo <lalo.g@bytedance.com>
2023-01-30 17:32:10 +0800

    fold

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -9,11 +9,9 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "gitcommit", "markdown" },
+  pattern = { "cmake" },
   callback = function()
-    -- vim.opt_local.wrap = true
-    -- vim.opt_local.spell = true
-    vim.opt_local.colorcolumn = "80"
+    vim.opt_local.foldmethod = "marker"
   end,
 })
 
