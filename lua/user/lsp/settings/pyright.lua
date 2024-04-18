ecef4d9391139b59dcee05a8020d455674466d60
lalo <lalo.g@bytedance.com>
2024-04-18 16:34:58 +0800

    chore: pyright config

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -5,13 +5,21 @@ return {
     python = {
       analysis = {
         extraPaths = {
-          "/Users/lalo/work/xbuild/install/RelWithDebInfo/Darwin-arm64/xsolution/lib",
-          "/Users/lalo/work/xbuild/install/RelWithDebInfo/Darwin-arm64/xsolution/python",
-          "/Users/lalo/work/xbuild/install/RelWithDebInfo/Darwin-arm64/xsolution/lib/python",
-          "/Users/lalo/work/xbuild/install/RelWithDebInfo/Darwin-arm64/xsolution/lib/python3.11",
-          "/Users/lalo/work/xbuild/install/RelWithDebInfo/Darwin-arm64/xsolution/lib/python3.11/site-packages",
+          -- "/Users/lalo/work/xbuild/install/RelWithDebInfo/Darwin-arm64/xsolution/lib",
+          -- "/Users/lalo/work/xbuild/install/RelWithDebInfo/Darwin-arm64/xsolution/python",
+          -- "/Users/lalo/work/xbuild/install/RelWithDebInfo/Darwin-arm64/xsolution/lib/python",
+          -- "/Users/lalo/work/xbuild/install/RelWithDebInfo/Darwin-arm64/xsolution/lib/python3.11",
+          -- "/Users/lalo/work/xbuild/install/RelWithDebInfo/Darwin-arm64/xsolution/lib/python3.11/site-packages",
+          "/Users/lalo/work/block/ds2/trunk/game",
+          "/Users/lalo/work/block/ds2/trunk/game/common",
+          "/Users/lalo/work/block/ds2/trunk/game/common/xcodebase",
+          "/Users/lalo/work/block/ds2/trunk/game/client",
+          "/Users/lalo/work/block/ds2/trunk/game/server",
         },
-        stubPath = "/Users/lalo/work/xbuild/install/RelWithDebInfo/Darwin-arm64/xsolution/api/pystub",
+        stubPath = "/Users/lalo/work/block/ds2/trunk/game/stub",
+        diagnosticSeverityOverrides = {
+          reportMissingModuleSource = "none"
+        }
       },
     },
   },
