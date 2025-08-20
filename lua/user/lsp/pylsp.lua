2ee0552891f134fc3945a875705c327366db382d
lalo <lalo.g@bytedance.com>
2025-08-20 21:40:30 +0800

    mod: pylsp 禁用跳转和补全

--- lua/user/lsp/pylsp.lua
+++ lua/user/lsp/pylsp.lua
@@ -18,6 +18,10 @@ vim.lsp.config('pylsp', {
     },
   },
   on_attach = function(client, bufnr)
-    client.server_capabilities.completionProvider = false
+    -- 关闭补全功能
+    client.server_capabilities.completionProvider = nil
+    -- 关闭跳转（定义/类型定义）
+    client.server_capabilities.definitionProvider = false
+    client.server_capabilities.typeDefinitionProvider = false
   end
 })
