9bca17064e7c3fe14c91c330ae4da37888b523fa
lalo <lalo.g@bytedance.com>
2025-08-28 14:29:35 +0800

    feat: A command switch between source/header

--- lua/user/lsp/clangd.lua
+++ lua/user/lsp/clangd.lua
@@ -8,3 +8,15 @@ vim.lsp.config('clangd', {
     offsetEncoding = 'utf-8',
   },
 })
+
+local prev_on_attach = vim.lsp.config.clangd.on_attach
+
+vim.lsp.config('clangd', {
+  on_attach = function(client, bufnr)
+    if prev_on_attach then
+      prev_on_attach(client, bufnr)
+    end
+    vim.api.nvim_buf_create_user_command(bufnr, 'A', 'LspClangdSwitchSourceHeader',
+      { desc = 'Switch between source/header' })
+  end,
+})
